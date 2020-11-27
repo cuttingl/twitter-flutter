35166586a5757e338dba2875bb13f39d6edf841d
laurent <laurent.cutting@gmail.com>
2020-11-27 17:50:39 +0100

    main view done // security on keys

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,20 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: MyHomePage(),
+    );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -30,9 +27,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            appBar: AppBar(
+          title: const Text('Twitter flutter test'),
+          bottom: const TabBar(
+            isScrollable: true,
+            tabs: [
+              Tab(text: 'Lecture auth keys'),
+              Tab(text: 'Premiere requete')
+            ],
+          ),
+        )));
   }
 }
--- lib/readkeys/readkeys.dart
+++ lib/readkeys/readkeys.dart
@@ -3,27 +3,22 @@ import 'dart:io';
 import 'dart:developer';
 import 'dart:async' show Future, Timer;
 
-
-
-
 main() async {
-    var file = File('keys.txt');
-    print(file);
-    var contents;
-    log("test avant cond");
-    if (await file.exists()) {
-      log("test apres cond");
-      // Read file
-      contents = await file.readAsString();
-      String resultat = contents.toString();
-      print(resultat);
-    }
-    else {
-      print("mauvais nom de fichier");
-    }
-      // Write file
-      /*var fileCopy = await File('keys.env').writeAsString(contents);
+  var file = File('keys.env');
+  print(file);
+  var contents;
+  log("test avant cond");
+  if (await file.exists()) {
+    log("test apres cond");
+    // Read file
+    contents = await file.readAsString();
+    String resultat = contents.toString();
+    print(resultat);
+  } else {
+    print("mauvais nom de fichier");
+  }
+  // Write file
+  /*var fileCopy = await File('keys.env').writeAsString(contents);
       print(await fileCopy.exists());
       print(await fileCopy.length());*/
-
 }
